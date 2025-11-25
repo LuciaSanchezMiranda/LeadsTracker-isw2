42ae66cfb1bb6c71d98148ef357fe5ef42379e13
LuciaSanchez <lucia.sanchez@vallegrande.edu.pe>
2025-11-25 16:04:58 -0500

    fix:app.py

--- app.py
+++ app.py
@@ -51,5 +51,5 @@ def index():
     
     return render_template('index.html', leads=leads)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
